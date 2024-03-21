1c2645543e0ab207444485ad3078e9aba9c695bb
LeGeNd <128361863+AnoxDx@users.noreply.github.com>
2024-03-21 18:14:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Get this value from my.telegram.org/apps
+##
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 STRING_SESSION = getenv("STRING_SESSION")
