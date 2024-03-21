da2aa05444a8267504fddfd5dbbe406e3c6ff9b6
LeGeNd <128361863+AnoxDx@users.noreply.github.com>
2024-03-21 18:12:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,21 @@ import asyncio
 from pyrogram.types import Message
 import config
 from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, CallbackQuery
 
 AnoxDx = Client(
   name="PyrogramBot",
   api_id=config.API_ID,
   api_hash=config.API_HASH,
-  bot_token=config.BOT_TOKEN
+  string_session=config.STRING_SESSION
 )
+START_BUTTONS = [
+    [
+        InlineKeyboardButton('⚡ ꜱᴜᴘᴘᴏʀᴛ ⚡', url='https://t.me/BotsDom')
+    ]
+]
+@AnoxDx.on_message(filters.command(["start"], ".") & (filters.me))
+ async def speed_test(client: Client, message: Message):
+    X = await message.reply_text("**Starting...**")
+    time.sleep(1.0)
+    await AnoxDx.edit_message_text(chat_id=message.chat.id, message_id=X.id, text="**TADDAAA :**\n\nIt's Your Own UserBot\n**#AnonymousUserBot**", reply_markup=InlineKeyboardMarkup(START_BUTTONS))
