720d4b301dccf5cf42e7431683db7461ed9e0643
LeGeNd <128361863+AnoxDx@users.noreply.github.com>
2024-03-22 17:28:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,18 @@ from pyrogram.types import Message
 import config
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, CallbackQuery
+from datetime import datetime
+import humanize
+from pyrogram.types import User
+from pyrogram import enums
+from prettytable import PrettyTable
+import datetime
+import math
+import random
+import time
+import uuid
+from random import randint
+from aiohttp import ClientSession
 
 AnoxDx = Client(
   name="PyrogramBot",
@@ -18,6 +30,13 @@ START_BUTTONS = [
         InlineKeyboardButton('⚡ ꜱᴜᴘᴘᴏʀᴛ ⚡', url='https://t.me/BotsDom')
     ]
 ]
+
+StartTime = time.time()
+START_TIME = datetime.now()
+CMD_HELP = {}
+SUDO_USER = SUDO_USERS
+clients = []
+ids = []
 
 @AnoxDx.on_message(filters.command(["start"], ".") & (filters.me))
 async def start(client, message):
@@ -41,5 +60,388 @@ async def fuck(client, message):
           \                _.•´
             \              (
               \**""")
+
+
+async def get_ub_chats(
+    client: Client,
+    chat_types: list = [
+        enums.ChatType.GROUP,
+        enums.ChatType.SUPERGROUP,
+        enums.ChatType.CHANNEL,
+    ],
+    is_id_only=True,
+):
+    ub_chats = []
+    async for dialog in client.get_dialogs():
+        if dialog.chat.type in chat_types:
+            if is_id_only:
+                ub_chats.append(dialog.chat.id)
+            else:
+                ub_chats.append(dialog.chat)
+        else:
+            continue
+    return ub_chats
+
+def ReplyCheck(message: Message):
+    reply_id = None
+
+    if message.reply_to_message:
+        reply_id = message.reply_to_message.id
+
+    elif not message.from_user.is_self:
+        reply_id = message.id
+
+    return reply_id
+
+
+def SpeedConvert(size):
+    power = 2 ** 10
+    zero = 0
+    units = {0: "", 1: "Kbit/s", 2: "Mbit/s", 3: "Gbit/s", 4: "Tbit/s"}
+    while size > power:
+        size /= power
+        zero += 1
+    return f"{round(size, 2)} {units[zero]}"
+
+
+def GetFromUserID(message: Message):
+    return message.from_user.id
+
+
+def GetChatID(message: Message):
+    return message.chat.id
+
+
+def GetUserMentionable(user: User):
+    if user.username:
+        username = "@{}".format(user.username)
+    else:
+        if user.last_name:
+            name_string = "{} {}".format(user.first_name, user.last_name)
+        else:
+            name_string = "{}".format(user.first_name)
+
+        username = "<a href='tg://user?id={}'>{}</a>".format(user.id, name_string)
+
+    return username
+async def edit_or_reply(message: Message, *args, **kwargs) -> Message:
+    xyz = (
+        message.edit_text
+        if bool(message.from_user and message.from_user.is_self or message.outgoing)
+        else (message.reply_to_message or message).reply_text
+    )
+    return await xyz(*args, **kwargs)
+
+@Client.on_message(filters.command(["help", "helpme"], ".") & filters.me)
+async def module_help(client: Client, message: Message):
+    cmd = message.command
+    help_arg = ""
+    bot_username = (await app.get_me()).username
+    if len(cmd) > 1:
+        help_arg = " ".join(cmd[1:])
+    elif not message.reply_to_message and len(cmd) == 1:
+        await message.edit("⚡️")
+        try:
+            nice = await client.get_inline_bot_results(bot=bot_username, query="helper")
+            await asyncio.gather(
+                message.delete(),
+                client.send_inline_bot_result(
+                    message.chat.id, nice.query_id, nice.results[0].id
+                ),
+            )
+        except BaseException as e:
+            print(f"{e}")
+            ac = PrettyTable()
+            ac.header = False
+            ac.title = "Zaid-UserBot Plugins"
+            ac.align = "l"
+            for x in split_list(sorted(CMD_HELP.keys()), 2):
+                ac.add_row([x[0], x[1] if len(x) >= 2 else None])
+            xx = await client.send_message(
+                message.chat.id,
+                f"```{str(ac)}```\n• @TheSupportChat × @TheUpdatesChannel •",
+                reply_to_message_id=ReplyCheck(message),
+            )
+            await xx.reply(
+                f"**Usage:** `.help broadcast` **To View Module Information**"
+            )
+            return
+
+    if help_arg:
+        if help_arg in CMD_HELP:
+            commands: dict = CMD_HELP[help_arg]
+            this_command = f"──「 **Help For {str(help_arg).upper()}** 」──\n\n"
+            for x in commands:
+                this_command += f"  •  **Command:** `.{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
+            this_command += "© @TheUpdatesChannel"
+            await edit_or_reply(
+                message, this_command, parse_mode=enums.ParseMode.MARKDOWN
+            )
+        else:
+            await edit_or_reply(
+                message,
+                f"`{help_arg}` **Not a Valid Module Name.**",
+            )
+
+
+@Client.on_message(filters.command(["plugins", "modules"], ".") & filters.me)
+async def module_helper(client: Client, message: Message):
+    cmd = message.command
+    help_arg = ""
+    if len(cmd) > 1:
+        help_arg = " ".join(cmd[1:])
+    elif message.reply_to_message and len(cmd) == 1:
+        help_arg = message.reply_to_message.text
+    elif not message.reply_to_message and len(cmd) == 1:
+        ac = PrettyTable()
+        ac.header = False
+        ac.title = "Zaid-UserBot Plugins"
+        ac.align = "l"
+        for x in split_list(sorted(CMD_HELP.keys()), 2):
+            ac.add_row([x[0], x[1] if len(x) >= 2 else None])
+        await edit_or_reply(
+            message, f"```{str(ac)}```\n• @TheSupportChat × @TheUpdatesChannel •"
+        )
+        await message.reply(
+            f"**Usage**:`.help broadcast` **To View Module details**"
+        )
+
+    if help_arg:
+        if help_arg in CMD_HELP:
+            commands: dict = CMD_HELP[help_arg]
+            this_command = f"──「 **Help For {str(help_arg).upper()}** 」──\n\n"
+            for x in commands:
+                this_command += f"  •  **Command:** `.{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
+            this_command += "© @TheUpdatesChannel"
+            await edit_or_reply(
+                message, this_command, parse_mode=enums.ParseMode.MARKDOWN
+            )
+        else:
+            await edit_or_reply(
+                message,
+                f"`{help_arg}` **Not a Valid Module Name.**",
+            )
+
+
+def add_command_help(module_name, commands):
+    if module_name in CMD_HELP.keys():
+        command_dict = CMD_HELP[module_name]
+    else:
+        command_dict = {}
+
+    for x in commands:
+        for y in x:
+            if y is not x:
+                command_dict[x[0]] = x[1]
+
+    CMD_HELP[module_name] = command_dict
+
+AFK = False
+AFK_REASON = ""
+AFK_TIME = ""
+USERS = {}
+GROUPS = {}
+
+
+def subtract_time(start, end):
+    subtracted = humanize.naturaltime(start - end)
+    return str(subtracted)
+
+def get_arg(message):
+    msg = message.text
+    msg = msg.replace(" ", "", 1) if msg[1] == " " else msg
+    split = msg[1:].replace("\n", " \n").split(" ")
+    if " ".join(split[1:]).strip() == "":
+        return ""
+    return " ".join(split[1:])
+
+def split_list(input_list, n):
+    n = max(1, n)
+    return [input_list[i: i + n] for i in range(0, len(input_list), n)]
+
+
+def human_time(*args, **kwargs):
+    secs = float(datetime.timedelta(*args, **kwargs).total_seconds())
+    units = [("day", 86400), ("hour", 3600), ("minute", 60), ("second", 1)]
+    parts = []
+    for unit, mul in units:
+        if secs / mul >= 1 or mul == 1:
+            if mul > 1:
+                n = int(math.floor(secs / mul))
+                secs -= n * mul
+            else:
+                n = secs if secs != int(secs) else int(secs)
+            parts.append("%s %s%s" % (n, unit, "" if n == 1 else "s"))
+    return ", ".join(parts)
+
+
+def random_interval():
+    rand_value = randint(14400, 43200)
+    delta = (time.time() + rand_value) - time.time()
+    return int(delta)
+
+
+def get_random_hex(chars=4):
+    my_hex = uuid.uuid4().hex[:chars]
+    return my_hex
+
+
+def get_mock_text(sentence):
+    new_sentence = ""
+    number = 0
+
+    for letter in sentence.lower():
+        if len(new_sentence) < 2:
+            random_number = random.randint(
+                0, 1
+            )
+            if random_number == 0:
+                new_sentence += letter.upper()
+            else:
+                new_sentence += letter
+        else:
+            if (
+                    new_sentence[number - 2].isupper()
+                    and new_sentence[number - 1].isupper()
+                    or new_sentence[number - 2].islower()
+                    and new_sentence[number - 1].islower()
+            ):
+                if new_sentence[
+                    number - 1
+                ].isupper():
+                    new_sentence += letter.lower()
+                else:
+                    new_sentence += letter.upper()
+            else:
+                random_number = random.randint(0, 1)
+                if random_number == 0:
+                    new_sentence += letter.upper()
+                else:
+                    new_sentence += letter
+
+        number += 1
+    return new_sentence
+
+@Client.on_message(
+    ((filters.group & filters.mentioned) | filters.private) & ~filters.me & ~filters.service, group=3
+)
+async def collect_afk_messages(bot: Client, message: Message):
+    if AFK:
+        last_seen = subtract_time(datetime.now(), AFK_TIME)
+        is_group = True if message.chat.type in ["supergroup", "group"] else False
+        CHAT_TYPE = GROUPS if is_group else USERS
+
+        if GetChatID(message) not in CHAT_TYPE:
+            text = (
+                f"`Beep boop. This is an automated message.\n"
+                f"I am not available right now.\n"
+                f"Last seen: {last_seen}\n"
+                f"Reason: ```{AFK_REASON.upper()}```\n"
+                f"See you after I'm done doing whatever I'm doing.`"
+            )
+            await bot.send_message(
+                chat_id=GetChatID(message),
+                text=text,
+                reply_to_message_id=ReplyCheck(message),
+            )
+            CHAT_TYPE[GetChatID(message)] = 1
+            return
+        elif GetChatID(message) in CHAT_TYPE:
+            if CHAT_TYPE[GetChatID(message)] == 50:
+                text = (
+                    f"`This is an automated message\n"
+                    f"Last seen: {last_seen}\n"
+                    f"This is the 10th time I've told you I'm AFK right now..\n"
+                    f"I'll get to you when I get to you.\n"
+                    f"No more auto messages for you`"
+                )
+                await bot.send_message(
+                    chat_id=GetChatID(message),
+                    text=text,
+                    reply_to_message_id=ReplyCheck(message),
+                )
+            elif CHAT_TYPE[GetChatID(message)] > 50:
+                return
+            elif CHAT_TYPE[GetChatID(message)] % 5 == 0:
+                text = (
+                    f"`Hey I'm still not back yet.\n"
+                    f"Last seen: {last_seen}\n"
+                    f"Still busy: ```{AFK_REASON.upper()}```\n"
+                    f"Try pinging a bit later.`"
+                )
+                await bot.send_message(
+                    chat_id=GetChatID(message),
+                    text=text,
+                    reply_to_message_id=ReplyCheck(message),
+                )
+
+        CHAT_TYPE[GetChatID(message)] += 1
+
+
+@AnoxDx.on_message(filters.command("afk", ".") & filters.me, group=3)
+async def afk_set(bot: Client, message: Message):
+    global AFK_REASON, AFK, AFK_TIME
+
+    cmd = message.command
+    afk_text = ""
+
+    if len(cmd) > 1:
+        afk_text = " ".join(cmd[1:])
+
+    if isinstance(afk_text, str):
+        AFK_REASON = afk_text
+
+    AFK = True
+    AFK_TIME = datetime.now()
+
+    await message.delete()
+
+
+@Client.on_message(filters.command("afk", "!") & filters.me, group=3)
+async def afk_unset(bot: Client, message: Message):
+    global AFK, AFK_TIME, AFK_REASON, USERS, GROUPS
+
+    if AFK:
+        last_seen = subtract_time(datetime.now(), AFK_TIME).replace("ago", "").strip()
+        await message.edit(
+            f"`While you were away (for {last_seen}), you received {sum(USERS.values()) + sum(GROUPS.values())} "
+            f"messages from {len(USERS) + len(GROUPS)} chats`"
+        )
+        AFK = False
+        AFK_TIME = ""
+        AFK_REASON = ""
+        USERS = {}
+        GROUPS = {}
+        await asyncio.sleep(5)
+
+    await message.delete()
+
+if AFK:
+   @Client.on_message(filters.me, group=3)
+   async def auto_afk_unset(bot: Client, message: Message):
+       global AFK, AFK_TIME, AFK_REASON, USERS, GROUPS
+
+       if AFK:
+           last_seen = subtract_time(datetime.now(), AFK_TIME).replace("ago", "").strip()
+           reply = await message.reply(
+               f"`While you were away (for {last_seen}), you received {sum(USERS.values()) + sum(GROUPS.values())} "
+               f"messages from {len(USERS) + len(GROUPS)} chats`"
+           )
+           AFK = False
+           AFK_TIME = ""
+           AFK_REASON = ""
+           USERS = {}
+           GROUPS = {}
+           await asyncio.sleep(5)
+           await reply.delete()
+
+
+add_command_help(
+    "afk",
+    [
+        [".afk", "Activates AFK mode with reason as anything after .afk\nUsage: ```.afk <reason>```"],
+        ["!afk", "Deactivates AFK mode."],
+    ],
+)
 
 AnoxDx.run()
