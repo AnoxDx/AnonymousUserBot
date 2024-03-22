e24739d8465250a19045bfad91bec680ffe294b4
LeGeNd <128361863+AnoxDx@users.noreply.github.com>
2024-03-22 16:51:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,8 +18,9 @@ START_BUTTONS = [
         InlineKeyboardButton('⚡ ꜱᴜᴘᴘᴏʀᴛ ⚡', url='https://t.me/BotsDom')
     ]
 ]
+
 @AnoxDx.on_message(filters.command(["start"], ".") & (filters.me))
- async def speed_test(client: Client, message: Message):
+ async def start(client: Client, message: Message):
     X = await message.reply_text("**Starting...**")
     time.sleep(1.0)
     await AnoxDx.edit_message_text(chat_id=message.chat.id, message_id=X.id, text="**TADDAAA :**\n\nIt's Your Own UserBot\n**#AnonymousUserBot**", reply_markup=InlineKeyboardMarkup(START_BUTTONS))
