3a7c6b0ac6d8c5969c4d18b3319989d233618649
LeGeNd <128361863+AnoxDx@users.noreply.github.com>
2024-03-22 16:53:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,9 +20,9 @@ START_BUTTONS = [
 ]
 
 @AnoxDx.on_message(filters.command(["start"], ".") & (filters.me))
- async def start(client: Client, message: Message):
-    X = await message.reply_text("**Starting...**")
-    time.sleep(1.0)
-    await AnoxDx.edit_message_text(chat_id=message.chat.id, message_id=X.id, text="**TADDAAA :**\n\nIt's Your Own UserBot\n**#AnonymousUserBot**", reply_markup=InlineKeyboardMarkup(START_BUTTONS))
+  async def start(client, message):
+     X = await message.reply_text("**Starting...**")
+     time.sleep(1.0)
+     await AnoxDx.edit_message_text(chat_id=message.chat.id, message_id=X.id, text="**TADDAAA :**\n\nIt's Your Own UserBot\n**#AnonymousUserBot**", reply_markup=InlineKeyboardMarkup(START_BUTTONS))
 
 AnoxDx.run()
