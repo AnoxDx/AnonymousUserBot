b98f30e7b5e0e3fa0b4b9550182816ecc6c17016
LeGeNd <128361863+AnoxDx@users.noreply.github.com>
2024-03-23 11:22:12 +0530

    Update and rename Anonymous/main.py to Anonymous/modules/start.py

---
+++ Anonymous/modules/start.py
@@ -0,0 +1,27 @@
+import random
+import importlib
+import time
+import asyncio
+from pyrogram.types import Message
+import config
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, CallbackQuery
+from Anonymous import Ano
+
+START_BUTTONS = [
+    [
+        InlineKeyboardButton('⚡ ꜱᴜᴘᴘᴏʀᴛ ⚡', url='https://t.me/BotsDom')
+    ]
+]
+
+CMD_HELP = {}
+
+@Ano.on_message(filters.command(["start"], ".") & (filters.me))
+async def start(client, message):
+   X = await message.reply_text("**Starting...**", reply_markup=InlineKeyboardMarkup(START_BUTTONS))
+   time.sleep(1.0)
+   await Ano.edit_message_text(chat_id=message.chat.id, message_id=X.id, text="**Got it :\n\nJust a moment**", reply_markup=InlineKeyboardMarkup(START_BUTTONS))
+   time.sleep(1.0)
+   await Ano.edit_message_text(chat_id=message.chat.id, message_id=X.id, text="**TADDAAA :**\n\nIt's Your Own UserBot\n**#AnonymousUserBot**", reply_markup=InlineKeyboardMarkup(START_BUTTONS))
+
+AnoxDx.run()
