8fdf9e70acb0cd69503beaef8cccee45b72d5c47
LeGeNd <128361863+AnoxDx@users.noreply.github.com>
2024-03-23 11:24:08 +0530

    Create fuck.py

---
+++ Anonymous/modules/fuck.py
@@ -0,0 +1,20 @@
+from Anonymous import Ano
+from pyrogram.types import Message
+from pyrogram import Client, filters
+import config
+
+@Ano.on_message(filters.command(["fuck"], ".") & (filters.me))
+async def fuck(client, message):
+   X = await message.reply_text("**Fucking...**")
+   time.sleep(1.0)
+   await Ano.edit_message_text(chat_id=message.chat.id, message_id=X.id, text="""**.                       /¯ )
+                      /¯  /
+                    /    /
+              /´¯/'   '/´¯¯•¸
+          /'/   /    /       /¨¯\
+        ('(   (   (   (  ¯~/'  ')
+         \                        /
+          \                _.•´
+            \              (
+              \**""")
+
