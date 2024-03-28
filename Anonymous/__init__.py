cfa5e1330aa25d9b21223e49775bf630af7d31ad
LeGeNd <128361863+AnoxDx@users.noreply.github.com>
2024-03-28 17:04:25 +0530

    Update __init__.py

--- Anonymous/__init__.py
+++ Anonymous/__init__.py
@@ -6,13 +6,7 @@ from pyrogram import Client
 
 import config
 
-loop = asyncio.get_event_loop()
-boot = time.time()
-
-mongo = MongoClient(config.MONGO_DB_URI)
-db = mongo.AFK
-
-SUDOERS = config.SUDO_USER
+SUDOERS = config.OWNER_ID
 
 Ano = Client(
     "Anonymous",
@@ -21,17 +15,3 @@ Ano = Client(
     session_string=config.STRING_SESSION,
 )
 
-
-async def initiate_bot():
-    global botid, botname, botusername
-    await Ano.start()
-    getme = await Ano.get_me()
-    botid = getme.id
-    botusername = (getme.username).lower()
-    if getme.last_name:
-        botname = getme.first_name + " " + getme.last_name
-    else:
-        botname = getme.first_name
-
-
-loop.run_until_complete(initiate_bot())
