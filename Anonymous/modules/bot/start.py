b99e4db9920bbdf5acaa66ca83ac877d96a17866
LeGeNd <128361863+AnoxDx@users.noreply.github.com>
2024-03-28 17:27:34 +0530

    Create start.py

---
+++ Anonymous/modules/bot/start.py
@@ -0,0 +1,43 @@
+from Anonymous import app, API_ID, API_HASH
+from config import OWNER_ID
+from pyrogram import filters
+import os
+import re
+import asyncio
+import time
+from pyrogram import *
+from pyrogram.types import * 
+
+PHONE_NUMBER_TEXT = (
+    "✘ Heya My Master👋!\n\n✘ I'm Your Assistant?\n\n‣ I can help you to host Your Left Clients.\n\n‣ This specially for Buzzy People's(lazy)\n\n‣ Now /clone {send your PyroGram String Session}"
+)
+ALIVE_PIC = 'https://graph.org/file/f7da95a365c0f89c85fb7.jpg'
+
+@app.on_message(filters.user(OWNER_ID) & filters.command("start"))
+async def hello(client: app, message):
+    buttons = [
+           [
+                InlineKeyboardButton("✘ ᴜᴘᴅᴀᴛᴇꜱ ᴄʜᴀɴɴᴇʟ", url="t.me/BotsDom"),
+            ],
+            [
+                InlineKeyboardButton("✘ ꜱᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ", url="t.me/OurTopics"),
+            ],
+            ]
+    reply_markup = InlineKeyboardMarkup(buttons)
+    await client.send_photo(message.chat.id, ALIVE_PIC, caption=PHONE_NUMBER_TEXT, reply_markup=reply_markup)
+
+@app.on_message(filters.user(OWNER_ID) & filters.command("clone"))
+async def clone(bot: app, msg: Message):
+    chat = msg.chat
+    text = await msg.reply("Usage:\n\n /clone session")
+    cmd = msg.command
+    phone = msg.command[1]
+    try:
+        await text.edit("Booting Your Client")
+                   # change this Directry according to ur repo
+        client = Client(name="Melody", api_id=API_ID, api_hash=API_HASH, session_string=phone, plugins=dict(root="Zaid/modules"))
+        await client.start()
+        user = await client.get_me()
+        await msg.reply(f"Your Client Has Been Successfully As {user.first_name} ✅.")
+    except Exception as e:
+        await msg.reply(f"**ERROR:** `{str(e)}`\nPress /start to Start again.")
