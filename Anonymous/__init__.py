450f6111d836e17e57bdcd6a9a83f81012649d70
LeGeNd <128361863+AnoxDx@users.noreply.github.com>
2024-03-28 17:21:45 +0530

    Update __init__.py

--- Anonymous/__init__.py
+++ Anonymous/__init__.py
@@ -1,16 +1,38 @@
-import asyncio
-import time
-
-from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
 from pyrogram import Client
-import config
+from config import API_ID, API_HASH, SUDO_USERS, OWNER_ID, BOT_TOKEN, STRING_SESSION1, STRING_SESSION2, STRING_SESSION3, STRING_SESSION4, STRING_SESSION5, STRING_SESSION6, STRING_SESSION7, STRING_SESSION8, STRING_SESSION9, STRING_SESSION10
+from datetime import datetime
+import time
+from aiohttp import ClientSession
 
-SUDOERS = config.OWNER_ID
+StartTime = time.time()
+START_TIME = datetime.now()
+CMD_HELP = {}
+SUDO_USER = SUDO_USERS
+clients = []
+ids = []
 
-Ano = Client(
-    "Anonymous",
-    config.API_ID,
-    config.API_HASH,
-    session_string=config.STRING_SESSION,
+SUDO_USERS.append(OWNER_ID)
+aiosession = ClientSession()
+
+if API_ID:
+   API_ID = API_ID
+else:
+   print("WARNING: API ID NOT FOUND USING AMAX API ⚡")
+   API_ID = "6435225"
+
+if API_HASH:
+   API_HASH = API_HASH
+else:
+   print("WARNING: API HASH NOT FOUND USING AMAX API ⚡")   
+   API_HASH = "4e984ea35f854762dcde906dce426c2d"
+
+if not BOT_TOKEN:
+   print("WARNING: BOT TOKEN NOT FOUND PLZ ADD ⚡")   
+
+app = Client(
+    name="app",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    in_memory=True,
 )
-
