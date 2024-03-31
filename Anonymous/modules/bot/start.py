e43b8269313280eac9d1c5692ca3e601571c34a9
LeGeNd <128361863+AnoxDx@users.noreply.github.com>
2024-03-31 19:51:12 +0530

    Update start.py

--- Anonymous/modules/bot/start.py
+++ Anonymous/modules/bot/start.py
@@ -8,10 +8,10 @@ import time
 from pyrogram import *
 from pyrogram.types import * 
 
-PHONE_NUMBER_TEXT = (
+START_TEXT = (
     "**Hey !\n\n> You are now the master of this bot, May you enjoy my superiority ⚡ \n\nThank You !**"
 )
-ALIVE_PIC = 'https://graph.org/file/f7da95a365c0f89c85fb7.jpg'
+START_PIC = 'https://graph.org/file/f7da95a365c0f89c85fb7.jpg'
 
 @app.on_message(filters.user(OWNER_ID) & filters.command("start"))
 async def hello(client: app, message):
@@ -23,21 +23,5 @@ async def hello(client: app, message):
                 InlineKeyboardButton("Chats", url="t.me/OurTopics"),
             ],
             ]
-    reply_markup = InlineKeyboardMarkup(buttons)
-    await client.send_photo(message.chat.id, ALIVE_PIC, caption=PHONE_NUMBER_TEXT, reply_markup=reply_markup)
-
-@app.on_message(filters.user(OWNER_ID) & filters.command("clone"))
-async def clone(bot: app, msg: Message):
-    chat = msg.chat
-    text = await msg.reply("Usage:\n\n /clone session")
-    cmd = msg.command
-    phone = msg.command[1]
-    try:
-        await text.edit("Booting Your Client")
-                   # change this Directry according to ur repo
-        client = Client(name="Melody", api_id=API_ID, api_hash=API_HASH, session_string=phone, plugins=dict(root="Zaid/modules"))
-        await client.start()
-        user = await client.get_me()
-        await msg.reply(f"Your Client Has Been Successfully As {user.first_name} ✅.")
-    except Exception as e:
-        await msg.reply(f"**ERROR:** `{str(e)}`\nPress /start to Start again.")
+    markups = InlineKeyboardMarkup(buttons)
+    await client.send_photo(message.chat.id, START_PIC, caption=START_TEXT, reply_markup=markups)
