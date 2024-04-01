a91c781499b6fe0bdafce5cce09f710533b4d638
LeGeNd <128361863+AnoxDx@users.noreply.github.com>
2024-04-01 21:32:41 +0530

    Update start.py

--- Anonymous/modules/basic/start.py
+++ Anonymous/modules/basic/start.py
@@ -6,7 +6,7 @@ from pyrogram.types import Message
 import config
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, CallbackQuery
-from Anonymous import Ano
+from Anonymous import app
 from Anonymous.modules.help import add_help_cmd
 
 START_BUTTONS = [
@@ -17,13 +17,13 @@ START_BUTTONS = [
 
 CMD_HELP = {}
 
-@Ano.on_message(filters.command(["start"], ".") & (filters.me))
+@app.on_message(filters.command(["start"], ".") & (filters.me))
 async def start(client, message):
    X = await message.reply_text("**Starting...**", reply_markup=InlineKeyboardMarkup(START_BUTTONS))
    time.sleep(1.0)
-   await Ano.edit_message_text(chat_id=message.chat.id, message_id=X.id, text="**Got it :\n\nJust a moment**", reply_markup=InlineKeyboardMarkup(START_BUTTONS))
+   await app.edit_message_text(chat_id=message.chat.id, message_id=X.id, text="**Got it :\n\nJust a moment**", reply_markup=InlineKeyboardMarkup(START_BUTTONS))
    time.sleep(1.0)
-   await Ano.edit_message_text(chat_id=message.chat.id, message_id=X.id, text="**TADDAAA :**\n\nIt's Your Own UserBot\n**#AnonymousUserBot**", reply_markup=InlineKeyboardMarkup(START_BUTTONS))
+   await app.edit_message_text(chat_id=message.chat.id, message_id=X.id, text="**TADDAAA :**\n\nIt's Your Own UserBot\n**#AnonymousUserBot**", reply_markup=InlineKeyboardMarkup(START_BUTTONS))
 
 add_help_cmd(
     "start",
