670cb19b441906ccf7b20d5ab0f7c6ca8a133b9a
LeGeNd <128361863+AnoxDx@users.noreply.github.com>
2024-04-01 21:29:39 +0530

    Update reaction.py

--- Anonymous/modules/basic/reaction.py
+++ Anonymous/modules/basic/reaction.py
@@ -1,9 +1,7 @@
 from Anonymous import app
 from pyrogram.types import Message
 from pyrogram import filters, Client
-from Anonymous.modules.help import add_help_cmd
 from Anonymous import SUDO_USER
-from Anonymous.modules.help import add_help_cmd
 
 @Client.on_message(filters.command(["react"], ".") & filters.me | filters.user(SUDO_USER))
 async def gs(client: Client, message: Message):
@@ -22,14 +20,3 @@ async def gs(client: Client, message: Message):
  await app.send_reaction(chat_id=user_s_to_send, message_id=user_s_to_send, emoji=emoji)
 
 
-add_help_cmd(
-    "react/send",
-    [
-        [
-            "reaction",
-            "Get your private invite link. [Need Admin]",
-        ],
-        ["react : replyy to user message", "to invite someone."],
-        ["send : mention chat id and message id with command"],
-    ],
-)
