fa6a92e70251c177b833c3600c93720cb01594f3
LeGeNd <128361863+AnoxDx@users.noreply.github.com>
2024-04-02 21:21:28 +0530

    Update start.py

--- Anonymous/modules/bot/start.py
+++ Anonymous/modules/bot/start.py
@@ -1,4 +1,4 @@
-import os
+/import os
 import re
 import asyncio
 import time
@@ -9,18 +9,16 @@ from pyrogram import filters
 
 
 START_TEXT = (
-    "**Hey !\n\n> You are now the master of this bot, May you enjoy my superiority ⚡ \n\nThank You !**"
+    "**Hey !\n\n>⚡ This is AmaX ! Your Assistant UB ! We are glad to assist you 1 ⚡ \n\n<u>Thank You !</u>**"
 )
-START_PIC = 'https://graph.org/file/f7da95a365c0f89c85fb7.jpg'
+START_PIC = 'https://graph.org/file/e6a9b462dd5d020d2af57.jpg'
 
 @app.on_message(filters.command("start") & filters.private)
 async def start(app, message):
     buttons = [
            [
                 InlineKeyboardButton("Channel", url="t.me/BotsDom"),
-            ],
-            [
-                InlineKeyboardButton("Chats", url="t.me/OurTopics"),
+                InlineKeyboardButton("Chats", url="t.me/OurTopics")
             ],
             ]
     await app.send_photo(message.chat.id, START_PIC, caption=START_TEXT, reply_markup=InlineKeyboardMarkup(buttons))
