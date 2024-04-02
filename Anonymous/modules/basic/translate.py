b3b80f76a5abbc380f375918df2a9e0079b62f32
LeGeNd <128361863+AnoxDx@users.noreply.github.com>
2024-04-02 20:57:41 +0530

    Update translate.py

--- Anonymous/modules/basic/translate.py
+++ Anonymous/modules/basic/translate.py
@@ -13,23 +13,23 @@ async def translate(client: Client, message: Message):
 
     try:
         lang = cmd[1] if len(message.command) > 1 else "en"
-        await message.edit_tex(f"**Translating in** `{lang}` . . .")
+        await message.edit_text(f"**Translating in** `{lang}` . . .")
         languages = list((gtl.get_supported_languages(as_dict=True)).values())
         if not lang in languages:
-            return await message.edit_tex("Bot doesn't support this language code, please try different one.", text_type=["mono"], delme=4)
+            return await message.edit_text("Bot doesn't support this language code, please try different one.", text_type=["mono"], delme=4)
         if (reply and reply.text):
             tdata = await translate(lang=lang, text=reply.text)
-            await message.edit_tex(f"**Translated to:** `{lang}`\n\n**Text: **`{tdata}`")
+            await message.edit_text(f"**Translated to:** `{lang}`\n\n**Text: **`{tdata}`")
         elif not reply and app.textlen(oldmsg) <= 4096:
             if len(message.command) <= 2:
-                return await message.edit_tex("Give me the language code with text to translate.", text_type=["mono"], delme=4)
+                return await message.edit_text("Give me the language code with text to translate.", text_type=["mono"], delme=4)
             text = m.text.split(None, 2)[2]
             tdata = await translate(lang=lang, text=text)
-            await message.edit_tex(f"**Translated to:** `{lang}`\n\n**Text:** `{tdata}`")
+            await message.edit_text(f"**Translated to:** `{lang}`\n\n**Text:** `{tdata}`")
         else:
-            await message.edit_tex("Something went wrong, please try again later !")
+            await message.edit_text("Something went wrong, please try again later !")
     except Exception as e:
-        await message.edit_tex("Something went wrong, please try again later !\n\n: {e}")
+        await message.edit_text("Something went wrong, please try again later !\n\n: {e}")
 
 
 async def translate(lang, text):
@@ -44,5 +44,5 @@ async def translatehelp(client: Client, message: Message):
     for keys, values in zip(langs_list.values(), langs_list.keys()):
         data.append(f"`{keys}` : `{values}`")
 
-    await message.edit_tex("**SUPPORTED LANGUAGES:**\n\n" + "\n".join(data))
+    await message.edit_text("**SUPPORTED LANGUAGES:**\n\n" + "\n".join(data))
         
