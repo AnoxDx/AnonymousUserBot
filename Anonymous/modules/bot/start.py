1134b8aa28fcedbc89bfcc3bb0c6ac3f399dd3a0
LeGeNd <128361863+AnoxDx@users.noreply.github.com>
2024-04-02 21:34:16 +0530

    Update start.py

--- Anonymous/modules/bot/start.py
+++ Anonymous/modules/bot/start.py
@@ -4,16 +4,16 @@ import asyncio
 import time
 from pyrogram import *
 from pyrogram.types import * 
-from Anonymous import app
-from pyrogram import filters
+from Anonymous import app, SUDO-USER
+from pyrogram import filters, Client
 
 
 START_TEXT = (
-    "**Hey !\n\n>⚡ This is AmaX ! Your Assistant UB ! We are glad to assist you 1 ⚡ \n\n<u>Thank You !</u>**"
+    f"**Hey {client.mention}  !\n\n>⚡ This is AmaX ! Your Assistant UB ! We are Glad to Assist You ⚡ \n\n<u>Thank You !</u>**"
 )
 START_PIC = 'https://graph.org/file/e6a9b462dd5d020d2af57.jpg'
 
-@app.on_message(filters.command("start") & filters.private)
+@app.on_message(filters.command("start") & (filters.me | filters.user(SUDO_USER))
 async def start(app, message):
     buttons = [
            [
