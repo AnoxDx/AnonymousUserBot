542c6605a3fa461622843b03cb3a2a82c16debda
LeGeNd <128361863+AnoxDx@users.noreply.github.com>
2024-04-02 09:41:44 +0530

    Update __init__.py

--- Anonymous/__init__.py
+++ Anonymous/__init__.py
@@ -18,7 +18,7 @@ app = Client(
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN,
-    plugins=dict(root="Anonymous/modules/bot")
+    plugins=dict(root="Anonymous/modules/bot"),
     in_memory=True,
 )
 
