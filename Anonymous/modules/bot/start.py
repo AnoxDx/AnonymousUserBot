b5b3fdf892e5f1266baa35ffff85e18446e0a3a5
LeGeNd <128361863+AnoxDx@users.noreply.github.com>
2024-04-02 12:32:23 +0530

    Update start.py

--- Anonymous/modules/bot/start.py
+++ Anonymous/modules/bot/start.py
@@ -4,7 +4,7 @@ import asyncio
 import time
 from pyrogram import *
 from pyrogram.types import * 
-from Anonymous import app, API_ID, API_HASH
+from Anonymous import app
 from config import OWNER_ID
 from pyrogram import filters
 
@@ -14,8 +14,8 @@ START_TEXT = (
 )
 START_PIC = 'https://graph.org/file/f7da95a365c0f89c85fb7.jpg'
 
-@app.on_message(filters.user(OWNER_ID) & filters.command("start"))
-async def hello(client: app, message):
+@app.on_message(filters.command("start") & filters.private)
+async def start(app, message):
     buttons = [
            [
                 InlineKeyboardButton("Channel", url="t.me/BotsDom"),
@@ -24,5 +24,4 @@ async def hello(client: app, message):
                 InlineKeyboardButton("Chats", url="t.me/OurTopics"),
             ],
             ]
-    markups = InlineKeyboardMarkup(buttons)
-    await app.send_photo(message.chat.id, START_PIC, caption=START_TEXT, reply_markup=markups)
+    await app.send_photo(message.chat.id, START_PIC, caption=START_TEXT, reply_markup=InlineKeyboardMarkup(buttons))
