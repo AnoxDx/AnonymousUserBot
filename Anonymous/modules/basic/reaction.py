1ec6450c56e39a4f915f802b1da4d939ddd2798e
LeGeNd <128361863+AnoxDx@users.noreply.github.com>
2024-04-02 10:21:00 +0530

    Update reaction.py

--- Anonymous/modules/basic/reaction.py
+++ Anonymous/modules/basic/reaction.py
@@ -4,14 +4,14 @@ from pyrogram import filters, Client
 from Anonymous import SUDO_USER
 
 @Client.on_message(filters.command(["react"], ".") & filters.me | filters.user(SUDO_USER))
-async def gs(client: Client, message: Message):
+async def react(client: Client, message: Message):
  emoji = message.text.split(" ", 1)[1]
- await app.send_reaction(chat_id=message.chat.id, message_id=message.reply_to_message, emoji=emoji)
+ await client.send_reaction(chat_id=message.chat.id, message_id=message.reply_to_message, emoji=emoji)
 
 @Client.on_message(filters.command(["send"], ".") & filters.me | filters.user(SUDO_USER))
-async def gs(client: Client, message: Message):
+async def react(client: Client, message: Message):
  user_s_to_send = message.text.split(" ", 1)[1]
  emoji = message.text.split(" ", 2)[2]
- await app.send_reaction(chat_id=user_s_to_send, message_id=user_s_to_send, emoji=emoji)
+ await client.send_reaction(chat_id=user_s_to_send, message_id=user_s_to_send, emoji=emoji)
 
 
