ea428c8f440057674fd4dca201c90ef04479b221
LeGeNd <128361863+AnoxDx@users.noreply.github.com>
2024-04-03 10:51:35 +0530

    Update sudodb.py

--- Anonymous/database/sudodb.py
+++ Anonymous/database/sudodb.py
@@ -10,7 +10,7 @@ from typing import Dict, List, Union
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message, User
 
-sudoersdb = mongodb.sudoers
+sudoersdb = db.sudoers
 
 SUDOERS = filters.user()
 
