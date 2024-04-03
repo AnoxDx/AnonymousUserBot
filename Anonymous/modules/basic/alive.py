f18602614829884ee89cce7ad76e01fcc8f38338
LeGeNd <128361863+AnoxDx@users.noreply.github.com>
2024-04-03 20:47:19 +0530

    Update alive.py

--- Anonymous/modules/basic/alive.py
+++ Anonymous/modules/basic/alive.py
@@ -2,22 +2,50 @@ import random
 import importlib
 import time
 import asyncio
+import os
+from platform import python_version
+from pyrogram import __version__ as versipyro
 from pyrogram.types import Message
 import config
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, CallbackQuery
-from Anonymous import app
+from Anonymous.modules.help.basic import edit_or_reply
+from Anonymous.helper.Pyt import ReplyCheck
 from Anonymous.modules.help import add_help_cmd
 
-CMD_HELP = {}
+alive_logo = ("https://telegra.ph//file/02469a3da0b5840bd9fe5.mp4") 
+StartTime = time.time()
+START_TIME = datetime.now()
 
-@Client.on_message(filters.command(["alive"], ".") & (filters.me))
-async def start(client, message):
-   X = await message.reply_text("**Starting...**")
-   time.sleep(1.0)
-   await client.edit_message_text(chat_id=message.chat.id, message_id=X.id, text="**Got it :\n\nJust a moment**")
-   time.sleep(1.0)
-   await client.edit_message_text(chat_id=message.chat.id, message_id=X.id, text="**TADDAAA :**\n\nIt's Your Own UserBot\n**#AnonymousUserBot**")
+@Client.on_message(filters.command(["alive", "awake"], ".") & filters.me)
+async def alive(client: Client, message: Message):
+    xx = await edit_or_reply(message, "⚡️")
+    await asyncio.sleep(2)
+    send = client.send_video if alive_logo.endswith(".mp4") else client.send_photo
+    uptime = await get_readable_time((time.time() - StartTime))
+    man = (
+        f"**[AmaX-UserBot](https://t.me/TheAmaX) is Up Alive.**\n\n"
+        f"⚡️ <b>Master :</b> {client.me.mention} \n"
+        f"⚡️ <b>Modules :</b> <code>{len(modules)} Modules</code> \n"
+        f"⚡️ <b>Bot Version :</b> <code>{BOT_VER}</code> \n"
+        f"⚡️ <b>Python Version :</b> <code>{python_version()}</code> \n"
+        f"⚡️ <b>Pyrogram Version :</b> <code>{versipyro}</code> \n"
+        f"⚡️ <b>Bot Uptime :</b> <code>{uptime}</code> \n\n"
+        f"    **📌 [𝗦𝘂𝗽𝗽𝗼𝗿𝘁](https://t.me/AmaXChats)** | **📌 [𝗖𝗵𝗮𝗻𝗻𝗲𝗹](https://t.me/TheAmaX)** | **📌 [𝗢𝘄𝗻𝗲𝗿](tg://user?id={client.me.id})**"
+    )
+    try:
+        await asyncio.gather(
+            xx.delete(),
+            send(
+                message.chat.id,
+                alive_logo,
+                caption=man,
+                reply_to_message_id=ReplyCheck(message),
+            ),
+        )
+    except BaseException:
+        await xx.edit(man, disable_web_page_preview=True)
+
 
 add_help_cmd(
     "alive",
